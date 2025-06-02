3b1f6a3978dbb416b69dcc6fed288f91248d5704
Anamul Hassan <itsanamulhassan@gmail.com>
2025-06-02 15:04:58 +0600

    update: module12

---
+++ module12/file01.js
@@ -0,0 +1,7 @@
+const a = 10;
+const add = (p1, p2) => p1 + p2;
+module.exports = {
+  a,
+  add,
+};
+// console.log(module);
---
+++ module12/utils/add.js
@@ -0,0 +1,1 @@
+export const add = (param1, param2) => param1 + param2;
