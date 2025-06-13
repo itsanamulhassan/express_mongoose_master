eada6dea1c5fffd92fdab2c1c99df7a6f40a41a2
Anamul Hassan <itsanamulhassan@gmail.com>
2025-06-14 02:02:13 +0600

    feat : implement mongodb connection and env file for better security (module14)

--- module14/dist/server.js
+++ module14/dist/server.js
@@ -13,8 +13,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+const { MongoClient, ServerApiVersion } = require("mongodb");
 let server;
 const port = 5000;
+//
+const uri = `mongodb+srv://${process.env.MODULE14_MONGODB_USERNAME}:${process.env.MODULE14_MONGODB_PASSWORD}@module14.ythvohv.mongodb.net/?retryWrites=true&w=majority&appName=Module14`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            yield client.connect();
+            // Send a ping to confirm a successful connection
+            yield client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            yield client.close();
+        }
+    });
+}
+run().catch(console.dir);
 const bootstrap = () => __awaiter(void 0, void 0, void 0, function* () {
     server = app_1.default.listen(port, () => {
         console.log(`Server listening on port ${port}`);
--- module14/src/server.ts
+++ module14/src/server.ts
@@ -1,8 +1,36 @@
 import app from "./app";
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 let server;
 
 const port = 5000;
+//
+
+const uri = `mongodb+srv://${process.env.MODULE14_MONGODB_USERNAME}:${process.env.MODULE14_MONGODB_PASSWORD}@module14.ythvohv.mongodb.net/?retryWrites=true&w=majority&appName=Module14`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const bootstrap = async () => {
   server = app.listen(port, () => {
