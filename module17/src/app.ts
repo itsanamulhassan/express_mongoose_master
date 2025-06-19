d301c99c0e49fedce758cbfb0ace246a5757e087
Anamul Hassan <itsanamulhassan@gmail.com>
2025-06-20 02:19:13 +0600

    feat(note): add endpoint for creating new note

--- module17/src/app.ts
+++ module17/src/app.ts
@@ -5,15 +5,33 @@ const app: Application = express();
 app.use(express.json());
 
 const noteSchema = new Schema({
-  title: String,
-  content: String,
+  title: { type: String, required: true, trim: true },
+  content: { type: String, default: "" },
+  category: {
+    type: String,
+    enum: ["personal", "work", "study", "other"],
+    default: "personal",
+  },
+  pinned: {
+    type: Boolean,
+    default: false,
+  },
+  tags: {
+    label: { type: String, required: true },
+    color: { type: String, default: "gray" },
+  },
 });
 
 const Note = model("Note", noteSchema);
 
-app.post("/create-note", async (req: Request, res: Response) => {
-  const note = new Note(req.body);
-  await note.save();
+app.post("/notes/create-note", async (req: Request, res: Response) => {
+  //Approach -1
+  //   const note = new Note(req.body);
+  //   await note.save();
+
+  // Approach -2
+  const note = await Note.create(req.body);
+
   res.status(201).json({
     success: true,
     message: "Note has been created successfully",
